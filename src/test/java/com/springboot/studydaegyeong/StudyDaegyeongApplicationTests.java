0261d5bf78541beffe55079549119e776e5a25d8
dhmk47 <dhmk47@naver.com>
2022-07-12 20:53:10 +0900

    init commit

---
+++ src/test/java/com/springboot/studydaegyeong/StudyDaegyeongApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.studydaegyeong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyDaegyeongApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
